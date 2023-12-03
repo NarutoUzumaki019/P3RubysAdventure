da7f66ba3b210ec9db613b1eb179612671ac926c
NarutoUzumaki019 <723740@student.dusd.net>
2023-12-03 10:31:23 -0800

    I worked on Ruby's Adventure
    I worked on Ruby's Adventure

---
+++ Ruby'sAdventureBarriosZachary/Assets/Scripts/DamageZone.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageZone : MonoBehaviour
+{
+   void OnTriggerEnter2D(Collider2D other)
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
